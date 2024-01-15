8042f5b2b961d2ea187a8b4446020d86612d547f
Allen Chen <chen6home@gmail.com>
2024-01-15 10:24:23 +0800

    help

--- solutions/two_d_lists.py
+++ solutions/two_d_lists.py
@@ -19,8 +19,8 @@ def sum_two_d_list(two_d_list):
 
 # TODO: Implement a function that prints each row of a two-dimensional list on a new line
 def print_rows(two_d_list):
-    for i in range (0,len(two_d_list)):
-        return two_d_list[i]
+    for row in two_d_list:
+        print(row)
 
 
 # TODO: Implement a function that checks whether a given value is present in any of the sublists within a two-dimensional list
@@ -50,15 +50,25 @@ def replace_in_two_d_list(two_d_list, old_value, new_value):
 
 # TODO: Implement a function that returns a new list containing only the first elements of each sublist in a two-dimensional list
 def first_elements(two_d_list):
-    list =[]
-    for i in range (0, len(two_d_list)):
-        list.append(two_d_list[i][0])
-    return list
+    result_list = []
+    if len(two_d_list) > 1:
+        for i in range(len(two_d_list)):
+            result_list.append(two_d_list[i][0])
+    return result_list
 
 # TODO: Implement a function that returns a list of lists, where each inner list contains the elements of the original sublists that are at even indices
 def even_elements_sublists(two_d_list):
-    pass
+    list = []
+    for i in range(0,len(two_d_list)):
+        while (i < len(two_d_list[0])):
+            list.append(two_d_list[i])
+            i=i+2
+    return list
 
 # TODO: Implement a function that concatenates all sublists in a two-dimensional list into a single list
 def concatenate_sublists(two_d_list):
-    pass
+    list = []
+    for row in two_d_list:
+        for item in row:
+            list.append(item)
+    return list
